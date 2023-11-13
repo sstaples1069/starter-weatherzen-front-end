456886f0a8cc5d3a458ff9a306d8327e11fa67c1
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 16:29:51 -0800

    initial commit after installs, first section of doThis

--- src/utils/api.js
+++ src/utils/api.js
@@ -44,3 +44,26 @@ async function fetchJson(url, options) {
     }
   }
 }
+
+const observations = [];
+
+function nextId() {
+  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
+  return uint32.toString(16);
+}
+
+export async function createObservation(observation, signal) {
+  const now = new Date().toISOString();
+  const newObservation = {
+    ...observation,
+    observation_id: nextId(),
+    created_at: now,
+    updated_at: now,
+  };
+  observations.push(newObservation);
+  return newObservation;
+}
+
+export async function listObservations(signal) {
+  return observations;
+}
