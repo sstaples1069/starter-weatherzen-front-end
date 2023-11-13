622359984c37741f663b93ba8601a3e16b6ed295
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 17:21:06 -0800

    updated to call the API to GET

--- src/utils/api.js
+++ src/utils/api.js
@@ -57,5 +57,10 @@ export async function createObservation(observation, signal) {
 }
 
 export async function listObservations(signal) {
-  return [];
+  const url = `${API_BASE_URL}/observations`;
+  const options = {
+    headers,
+    signal
+  }
+  return await fetchJson(url, options)
 }
